e01e4dfd0d7d2280620542753bad874b112f6b84
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-10 20:08:33 -0300

    se obtiene el marcador modificado, se hace la update y se dispara el emit

--- models/sockets.js
+++ models/sockets.js
@@ -23,6 +23,12 @@ class Sockets {
 
                 socket.broadcast.emit( 'new-marker', marker );
             })
+
+            socket.on('update-marker', (marker)=>{
+                this.markers.updateMarker(marker);
+                socket.broadcast.emit('update-marker', marker);
+            });
+
         })
 
     }
